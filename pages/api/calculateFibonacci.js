26d8f19e44e91bd1cd27f44e4fa48273107bab1a
Satyam Bharia <bhariasatyam007@gmail.com>
2023-08-04 17:57:41 -0400

    API for calculating and storing fibonacci

--- pages/api/calculateFibonacci.js
+++ pages/api/calculateFibonacci.js
@@ -1,1 +1,63 @@
-//API for calculating fibonacci numbers
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+
+    if(req.method == 'POST'){
+        const {n} = req.body;
+        await calculateFibonacci(parseInt(n));
+        res.status(200).json({result:"OK"});
+    }
+
+    else{
+        res.status(405).end();
+    }
+}
+
+async function calculateFibonacci(n) {
+    const existingNumbers = await prisma.fibonacci.findMany({
+        orderBy: {id: "asc"},
+    });
+
+    let count = existingNumbers.length;
+
+    if(count<n) {
+        let num1 = 0, num2 = 1;
+        if(count >=2){
+            num1 = existingNumbers[count-2].value;
+            num2 = existingNumbers[count-1].value;
+
+            for(let i=count; i<n; i++){
+                let next = num1 + num2;
+                await prisma.fibonacci.create({ data: { value: next } });
+                num1 = num2;
+                num2 = next;
+            }
+        }
+
+        else if(count == 0 && n == 1){
+            await prisma.fibonacci.create({ data: { value: num1 } });
+        }
+        else if(count == 0 && n >= 2){
+            await prisma.fibonacci.create({ data: { value: num1 } });
+            await prisma.fibonacci.create({ data: { value: num2 } });
+            for(let i=2; i<n; i++){
+                let next = num1 + num2;
+                await prisma.fibonacci.create({ data: { value: next } });
+                num1 = num2;
+                num2 = next;
+            }
+        }
+        else if(count == 1 && n>=2){
+            await prisma.fibonacci.create({ data: { value: num2 } });
+            for(let i=2; i<n; i++){
+                let next = num1 + num2;
+                await prisma.fibonacci.create({ data: { value: next } });
+                num1 = num2;
+                num2 = next;
+            }
+        }
+
+    }
+}
